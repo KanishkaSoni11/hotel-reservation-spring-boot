db61376d4a5cf50a6eb260c5ef5dc6608e93c439
kanishkasoni <soni.kan@northeastern.edu>
2023-04-02 13:56:43 -0400

    react and cors

--- src/main/java/com/example/hotelreservation/HotelReservationApplication.java
+++ src/main/java/com/example/hotelreservation/HotelReservationApplication.java
@@ -2,6 +2,9 @@ package com.example.hotelreservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HotelReservationApplication {
@@ -9,5 +12,15 @@ public class HotelReservationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HotelReservationApplication.class, args);
 	}
+	@Bean
+	public WebMvcConfigurer configure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry reg) {
+				reg.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+
 
 }
--- src/main/java/com/example/hotelreservation/controller/StaffController.java
+++ src/main/java/com/example/hotelreservation/controller/StaffController.java
@@ -5,6 +5,7 @@ import com.example.hotelreservation.service.StaffService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +20,12 @@ public class StaffController {
     @Autowired
     private StaffService staffService;
 
-    @GetMapping("/staff")
-    public List<Staff> getAllStaff(){
-        logger.error("Fetching all staff members");
-        return staffService.getAllStaff();
-
-    }
+//    @GetMapping("/staff")
+//    public List<Staff> getAllStaff(){
+//        logger.error("Fetching all staff members");
+//        return staffService.getAllStaff();
+//
+//    }
 
     @GetMapping("/staff/{staffId}")
     public Staff getStaffById(@PathVariable int staffId){
