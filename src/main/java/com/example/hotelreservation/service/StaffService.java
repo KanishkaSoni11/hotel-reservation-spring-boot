2518b245f5f4d787ebcba1e89a4dc7c4bb78313d
kanishkasoni <soni.kan@northeastern.edu>
2023-04-02 11:08:20 -0400

    staff API

--- src/main/java/com/example/hotelreservation/service/StaffService.java
+++ src/main/java/com/example/hotelreservation/service/StaffService.java
@@ -1,4 +1,11 @@
 package com.example.hotelreservation.service;
 
+import com.example.hotelreservation.model.Staff;
+
+import java.util.List;
+
 public interface StaffService {
+
+    public Staff getStaffById(int staffId);
+    public List<Staff> getAllStaff();
 }
